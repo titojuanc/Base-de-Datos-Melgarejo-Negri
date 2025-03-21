f548419f4eee4eb6103f3ad06fa9a3bbeb65584e
titojuanc <167209613+titojuanc@users.noreply.github.com>
2025-03-21 08:41:47 -0300

    Ejercicios 5 y 6 completados.

--- BDD_funciones_extra_14_03.sql
+++ BDD_funciones_extra_14_03.sql
@@ -58,4 +58,40 @@ end//
 delimiter ;
 select EjExtra4("S18_4409", 10100);
 drop function EjExtra4;
-	
+
+/*5_Ex*/
+
+delimiter //
+create function cant_stock (codigoProd varchar(45)) returns text deterministic
+begin
+	declare stock int;
+    set stock = (select quantityInStock from products where productCode=codigoProd);
+    if stock>5000 then
+		return "Sobrestock";
+	end if;
+	if stock < 5000 and stock > 1000 then
+		return "Stock adecuado";
+	end if;
+    if stock < 1000 then
+		return "Bajo stock";
+    end if;
+end//
+delimiter ;
+drop function cant_stock;
+select cant_stock("S12_1666");
+
+/*Ej6*/
+delimiter //
+create function Ej6(anio varchar(45))returns text deterministic
+begin
+	declare top1 varchar(45);
+    declare top2 varchar(45);
+    declare top3 varchar(45);
+	select productName into top1 from products join orderdetails on products.productCode=orderdetails.productCode join orders on orderdetails.orderNumber=orders.orderNumber where anio = year(shippedDate) group by products.productCode order by sum(quantityOrdered) desc limit 1;
+    select productName into top2 from products join orderdetails on products.productCode=orderdetails.productCode join orders on orderdetails.orderNumber=orders.orderNumber where anio = year(shippedDate) group by products.productCode order by sum(quantityOrdered) desc limit 1 offset 1;
+    select productName into top3 from products join orderdetails on products.productCode=orderdetails.productCode join orders on orderdetails.orderNumber=orders.orderNumber where anio = year(shippedDate) group by products.productCode order by sum(quantityOrdered) desc limit 1 offset 2;
+	return
+		concat_ws(',',top1,top2,top3);
+end//
+delimiter ;
+select Ej6("2003");	
